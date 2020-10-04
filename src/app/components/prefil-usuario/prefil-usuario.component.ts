ed9da90606dfd63af3f7d43b8e56befdb0701a71
Bryam11 <xavierchuchuca18@gmail.com>
2020-10-03 19:40:52 -0500

    comit

--- src/app/components/prefil-usuario/prefil-usuario.component.ts
+++ src/app/components/prefil-usuario/prefil-usuario.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PersonaControllerService } from 'app/Rest';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-prefil-usuario',
@@ -9,14 +10,27 @@ import { PersonaControllerService } from 'app/Rest';
 export class PrefilUsuarioComponent implements OnInit {
   ListaPersonas: any = [];
 
-  constructor(private personaServicio: PersonaControllerService) { 
+  constructor(private personaServicio: PersonaControllerService, private routes: ActivatedRoute, private router: Router) {
 
   }
 
   ngOnInit(): void {
- 
+    this.routes.paramMap.subscribe(params => {
+      if (params.has('nombre')) {
+
+        // tslint:disable-next-line: max-line-length
+        this.personaServicio.verusuarioUsingGET(params.get('nombre')).subscribe(data => {
+         this.ListaPersonas= data;
+         console.log(this.ListaPersonas)
+        })
+      }
+      else {
+        window.alert('no se ha encontrado ningun registro');
+      }
+
+    })
   }
-
- 
-
+  navegarpublicaion() {
+    this.router.navigate(['/publicaciones'])
+  }
 }
