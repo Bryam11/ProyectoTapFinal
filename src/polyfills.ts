97f7deb01615ad98f0212793877bb7775b6cd6c9
Bryam11 <xavierchuchuca18@gmail.com>
2020-10-13 18:12:29 -0500

    agregar dependencias para utilizar el jdk

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -60,7 +60,7 @@ import 'core-js/es/reflect';
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
-
+if (typeof (window as any).global === 'undefined') {(window as any).global = window};
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
