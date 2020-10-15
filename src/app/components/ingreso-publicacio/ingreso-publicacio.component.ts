7120e3f2c390a09f37b3298b29d2c9766c1c616c
Bryam11 <xavierchuchuca18@gmail.com>
2020-10-14 19:16:32 -0500

    comet of code

--- src/app/components/ingreso-publicacio/ingreso-publicacio.component.ts
+++ src/app/components/ingreso-publicacio/ingreso-publicacio.component.ts
@@ -14,7 +14,7 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./ingreso-publicacio.component.css']
 })
 export class IngresoPublicacioComponent implements OnInit {
-//DECLARACION DE VARIABLES
+  // DECLARACION DE VARIABLES
   showMensaje = false;
   tipoSeleccionada = '';
   publicacion: Publicaciones = {};
@@ -32,7 +32,7 @@ export class IngresoPublicacioComponent implements OnInit {
     this.usuario.usuario = localStorage.getItem('user');
   }
 
-  //VALIDAR COMBO BOX DE COUNTRY
+  // VALIDAR COMBO BOX DE COUNTRY
   lenguajesChangeListener() {
     if (this.tipoSeleccionada != null && this.tipoSeleccionada != 'Seleccione...') {
       this.showMensaje = true;
@@ -41,29 +41,29 @@ export class IngresoPublicacioComponent implements OnInit {
     }
   }
 
-  //METODO PARA LISTAR LENGUAJES DE PROGRAMACION
+  // METODO PARA LISTAR LENGUAJES DE PROGRAMACION
   Listarlenjuages() {
     this.mostrarToast();
     this.lenguajeservice.listarlenguajesUsingGET().subscribe(data => {
-    this.listaLenguajes = data;
+      this.listaLenguajes = data;
     })
   }
 
-//METODO PARA MOSTRAR MENSAJE TOAST DE BIENVENIDA (BIENVENIDO PARA CREAR PUBLICACION)
+  // METODO PARA MOSTRAR MENSAJE TOAST DE BIENVENIDA (BIENVENIDO PARA CREAR PUBLICACION)
   mostrarToast() {
     var toast = document.getElementById('mitoast');
     toast.className = 'mostrar';
     setTimeout(function () { toast.className = toast.className.replace('mostrar', ''); }, 6000);
   }
 
-  //METODO PARA MOSTRAR MENSAJE TOAST DE CONFIRMACION (PUBLICACION CREADA CORRECTAMENTE)
+  // METODO PARA MOSTRAR MENSAJE TOAST DE CONFIRMACION (PUBLICACION CREADA CORRECTAMENTE)
   mostrarToastPubli() {
     var toast = document.getElementById('mitoastpubli');
     toast.className = 'mostrar';
     setTimeout(function () { toast.className = toast.className.replace('mostrar', ''); }, 6000);
   }
 
-  //METODO PARA MOSTRAR MENSJE TOAST DE VALIDACION (LLENAR TODOS LOS CAMPOS)
+  // METODO PARA MOSTRAR MENSJE TOAST DE VALIDACION (LLENAR TODOS LOS CAMPOS)
   mostrarToastVali() {
     var toast = document.getElementById('mitoastvali');
     toast.className = 'mostrar';
@@ -71,27 +71,29 @@ export class IngresoPublicacioComponent implements OnInit {
   }
 
 
-//METODO PARA CERRAR EL MENSAJE TOAST
+  // METODO PARA CERRAR EL MENSAJE TOAST
   cerrarToast() {
     var toast = document.getElementById('mitoast');
     toast.className = 'cerrar';
     toast.className = toast.className.replace('cerrar', '');
   }
 
-  //METODO PARA GUARDAR UNA PUBLICACION NUEVA
+  // METODO PARA GUARDAR UNA PUBLICACION NUEVA
   guardarPublicacion() {
-if(this.publicacion.ide == undefined ||this.publicacion.codigo == undefined ||  this.publicacion.descripcion == undefined 
-||this.publicacion.fecha == undefined || this.publicacion.lenguajeProgra  == undefined || this.publicacion.titulo== undefined 
-|| this.usuario.usuario == undefined){
-this.mostrarToastVali();
-}else{
-  this.publicacion.lenguajeProgra = this.LenguajeSeleccionado;
-  this.personaservice.anadirPublicacionPersonaUsingPUT(this.publicacion.ide, this.publicacion.codigo, this.publicacion.descripcion, this.publicacion.fecha, this.publicacion.lenguajeProgra, this.publicacion.titulo, this.usuario.usuario).subscribe(data => {
-  this.mostrarToastPubli();
-  })
-}
+    if (this.publicacion.ide == undefined || this.publicacion.codigo == undefined || this.publicacion.descripcion == undefined
+      || this.publicacion.fecha == undefined || this.publicacion.lenguajeProgra == undefined || this.publicacion.titulo == undefined
+      || this.usuario.usuario == undefined) {
+      this.mostrarToastVali();
+    } else {
+      this.publicacion.lenguajeProgra = this.LenguajeSeleccionado;
+      this.personaservice.anadirPublicacionPersonaUsingPUT(this.publicacion.ide, this.publicacion.codigo, this.publicacion.descripcion, this.publicacion.fecha, this.publicacion.lenguajeProgra, this.publicacion.titulo, this.usuario.usuario).subscribe(data => {
+        this.mostrarToastPubli();
+      })
+    }
   }
 
+  
+  // METODO PARA ABRIR UNA VENTANA Y VISUALIZAR LA PUBLICACION ANTE DE GUARDARLA
   open(content, type) {
     if (type === 'sm') {
       console.log('aici');
@@ -109,6 +111,7 @@ this.mostrarToastVali();
     }
   }
 
+  // METODO PARA UTILIZAR EL ESC Y CERRARR LA VENTANA DE PREVISUALIZACION DE LA VENTANA
   private getDismissReason(reason: any): string {
     if (reason === ModalDismissReasons.ESC) {
       return 'by pressing ESC';
